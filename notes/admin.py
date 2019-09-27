2a8cdf9418b2c37cd021d9380c8a4c275fc8bd89
cocoitali <cmburatto@gmail.com>
2019-09-27 10:48:21 -0600

    Migrations with new fields

--- notes/admin.py
+++ notes/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Note
-admin.site.register(Note)
+admin.site.register(Note, NoteAdmin)
 
 # If you want to register more models, you can do so with additional register() calls:
 # admin.site.register(Note)
--- notes/models.py
+++ notes/models.py
@@ -6,3 +6,8 @@ class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    class NoteAdmin(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')

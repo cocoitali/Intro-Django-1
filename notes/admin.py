a3d28b3d46a7d25931d893055fa8d1ad194d2455
cocoitali <cmburatto@gmail.com>
2019-09-27 10:43:30 -0600

    Registered Note model

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Note
+admin.site.register(Note)
+
+# If you want to register more models, you can do so with additional register() calls:
+# admin.site.register(Note)
+# admin.site.register(PersonalNote)   # etc.
